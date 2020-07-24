d1d55cf251be9e5bf0fbbc1c6278b8100d06e790
roee030 <roee.angel93@gmail.com>
2020-07-24 13:55:58 +0300

    boot a new project

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Services',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Products',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact US',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
