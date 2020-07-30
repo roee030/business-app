ed6af27f5f7698c808ccc9e9650c77230fbf6504
roee030 <roee.angel93@gmail.com>
2020-07-30 18:25:41 +0300

    more style

--- src/components/Contact-popup/Contact_popup.js
+++ src/components/Contact-popup/Contact_popup.js
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from 'react';
 import './Contact_popup.css';
 import logo from '../../images/manager-photo.jpeg';
-import stars from '../../images/5stars.png'; 
+import stars from '../../images/5stars.png';
 export default function Contact_popup() {
-    return (
-        <div className='contact-warper'>
-            <img className="contact-img"src={logo} alt='logo'></img>
-            <p className="contact-name">זיו ודולב</p>
-            <hr></hr>
-            <img className="contact-5star" src={stars} alt='stars'></img>
-            <button className="contact-PN" >לחץ כאן לשיחה קולית</button>
-            <button className="contact-WA">לחץ כאן לשליחת הודעה ואטספ</button>
-        </div>
-    )
+	return (
+		<div className="contact-warper">
+			<img className="contact-img" src={logo} alt="logo"></img>
+			<p className="contact-name">זיו ודולב</p>
+			<img className="contact-5star" src={stars} alt="stars"></img>
+			<button className="contact-PN">לחץ כאן לשיחה קולית</button>
+			<button className="contact-WA">לחץ כאן לשליחת הודעה ואטספ</button>
+		</div>
+	);
 }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,43 +11,54 @@ import alarm from '../../images/Security-alarm.jpg';
 import ContactFix from '../../components/Contact-popup/Contact_popup.js';
 
 export default function Hero() {
-    return (
-            <div className='hero'>
-                
-                <OwlCarousel
-                className='owl-theme'
-                items='1'
-                autoplay={true}
-                loop={true}
-                dots	={false}
-                >
-                    <div className="item">
-                        <div className="wrapper">    
-                            <h1 className="title-item"> .מצבעים קריאות שירות של שקע עובד ועד בניה של בתים שיפוצים ותוספות</h1>
-                            <img className="img-item" src={electricity}></img>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="wrapper">    
-                                <h1 className="title-item"> .אנו מתרינים, מתכנתים ומוכרים ציוד חכם לבית </h1>
-                            <img className="img-item" src={controlling}></img>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="wrapper">    
-                                    <h1 className="title-item">.אנו דואגים לכיסוי רשת מלא בכל הבית ובחוץ </h1>
-                            <img className="img-item" src={network}></img>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="wrapper">    
-                            <h1 className="title-item"> .מכוונים את המערכת כך שהבית עובד בשבילך ולא אתה בשבילו</h1>
-                            <img className="img-item" src={alarm}></img>
-                        </div>
-                    </div>
-        
-                </OwlCarousel>
-                
-            </div>
-    )
+	return (
+		<div className="hero">
+			<OwlCarousel
+				className="owl-theme"
+				items="1"
+				autoplay={true}
+				loop={true}
+				dots={false}
+			>
+				<div className="item">
+					<div className="wrapper">
+						<h1 className="title-item">
+							{' '}
+							.מצבעים קריאות שירות של שקע עובד ועד בניה של בתים
+							שיפוצים ותוספות
+						</h1>
+						<img className="img-item" src={electricity}></img>
+					</div>
+				</div>
+				<div className="item">
+					<div className="wrapper">
+						<h1 className="title-item">
+							{' '}
+							.אנו מתרינים, מתכנתים ומוכרים ציוד חכם לבית{' '}
+						</h1>
+						<img className="img-item" src={controlling}></img>
+					</div>
+				</div>
+				<div className="item">
+					<div className="wrapper">
+						<h1 className="title-item">
+							.אנו דואגים לכיסוי רשת מלא בכל הבית ובחוץ{' '}
+						</h1>
+						<img className="img-item" src={network}></img>
+					</div>
+				</div>
+				<div className="item">
+					<div className="wrapper">
+						<h1 className="title-item">
+							{' '}
+							.מכוונים את המערכת כך שהבית עובד בשבילך ולא אתה
+							בשבילו
+						</h1>
+						<img className="img-item" src={alarm}></img>
+					</div>
+				</div>
+			</OwlCarousel>
+			<ContactFix className="contact" />
+		</div>
+	);
 }
