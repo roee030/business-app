df8a3606c7f69cba8b5dfbb95becbe36125c8346
roee030 <roee.angel93@gmail.com>
2020-08-02 10:43:19 +0300

    style the about section

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar.js';
 import Hero from './components/Hero/Hero.js';
 import Services from './components/Services/Services.js';
+import About from './components/About/About.js';
 function App() {
-
-  return (
-    <div className="App">
-      <Navbar/>
-      <Hero/>
-      <Services/>
-     
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+			<Hero />
+			<About />
+			<Services />
+		</div>
+	);
 }
 
 export default App;
