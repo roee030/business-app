25588f8c2e0ba6f91a10188e5e503221dcdc7a69
roee030 <roee.angel93@gmail.com>
2020-08-02 18:23:42 +0300

    stick the connection info to the top

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Hero from './components/Hero/Hero.js';
 import Services from './components/Services/Services.js';
 import About from './components/About/About.js';
 import Footer from './components/Contact-section/Contact.js';
+import ConnectionNavbar from './components/Connection/Connection.js';
 function App() {
 	return (
 		<div className="App">
+			<ConnectionNavbar />
 			<Navbar />
 			<Hero />
 			<Services />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {MenuItems} from './MenuItems.js';
+import { MenuItems } from './MenuItems.js';
 import './Navbar.css';
 import Connection from '../Connection/Connection.js';
 import Logo from '../../fontsAndIcons/logo.png';
@@ -7,36 +7,47 @@ import Bars from '../../fontsAndIcons/bars-solid.svg';
 import Close from '../../fontsAndIcons/times-solid.svg';
 
 class Navbar extends Component {
-    
-    state = {
-        clicked: false,
-    }
-    
-    render() {
-        const handleClick = () => {
-            this.setState({clicked: !this.state.clicked})
-        }
-        return(
-            <>
-            <Connection/>
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo"> <img className="Logo" src={Logo} alt="logo"></img></h1>
-                <ul className={ this.state.clicked ? 'nav-menu active' :'nav-menu' }>
-                    {MenuItems.map((item, index) => {
-                        return(
-                            <li key={index}>
-                                <a className={item.cName} href={item.url}>
-                                    {item.title}
-                                </a>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <img className="menu-icon" onClick={handleClick} src={this.state.clicked ? Close : Bars} alt="sidebar"></img>
-            </nav>
-            </>
-        )
-    }
+	state = {
+		clicked: false,
+	};
+
+	render() {
+		const handleClick = () => {
+			this.setState({ clicked: !this.state.clicked });
+		};
+		return (
+			<>
+				<Connection />
+				<nav className="NavbarItems">
+					<h1 className="navbar-logo">
+						{' '}
+						<img className="Logo" src={Logo} alt="logo"></img>
+					</h1>
+					<ul
+						className={
+							this.state.clicked ? 'nav-menu active' : 'nav-menu'
+						}
+					>
+						{MenuItems.map((item, index) => {
+							return (
+								<li key={index}>
+									<a className={item.cName} href={item.url}>
+										{item.title}
+									</a>
+								</li>
+							);
+						})}
+					</ul>
+					<img
+						className="menu-icon"
+						onClick={handleClick}
+						src={this.state.clicked ? Close : Bars}
+						alt="sidebar"
+					></img>
+				</nav>
+			</>
+		);
+	}
 }
 
 export default Navbar;
